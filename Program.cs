54cadcb2bb8f1db69050b9c9556fe0ce9ee562ff
citro <citro@MORGANLAPTOP>
2024-10-29 21:19:56 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace PrimeNumberTester
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number that you want to be checked:");
+            int number = int.Parse(Console.ReadLine());
+            int testNumber = number / 2;
+            int flag = 0;
+            for (int i = 2; i <= testNumber; i++)
+            {
+                if (testNumber % i == 0)
+                {
+                    Console.Write("Number is not Prime.");
+                    flag = 1;
+                    break;
+                }
+            }
+            if (flag == 0)
+                Console.WriteLine("Number is Prime");
+        }
+    }
+}
